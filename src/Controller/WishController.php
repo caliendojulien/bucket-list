2f4d39a88b7602cfe7b8d23f6e9ef1ebb14a78ba
Caliendo Julien <caliendo@hotmail.fr>
2022-09-29 11:07:43 +0200

    TP du module 6

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Wish;
+use App\Form\WishType;
 use App\Repository\WishRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -28,11 +31,37 @@ class WishController extends AbstractController
         requirements: ['id' => '\d+']
     )]
     public function detail(
-        Wish           $id
+        Wish $id
     ): Response
     {
         return $this->render('wish/detail.html.twig', [
             "wish" => $id
+        ]);
+    }
+
+    #[Route('/wish/ajout',
+        name: 'wish_ajout',
+    )]
+    public function ajout(
+        Request                $request,
+        EntityManagerInterface $entityManager
+    ): Response
+    {
+        $wish = new Wish();
+        $formWish = $this->createForm(WishType::class, $wish);
+
+        $formWish->handleRequest($request);
+
+        if ($formWish->isSubmitted() && $formWish->isValid()) {
+            $wish->setDateCreated(new \DateTime());
+            $wish->setIsPublished(true);
+            $entityManager->persist($wish);
+            $entityManager->flush();
+            return $this->redirectToRoute('wish_detail', ["id" => $wish->getId()]);
+        }
+
+        return $this->render('wish/ajout.html.twig', [
+            "formWish" => $formWish->createView()
         ]);
     }
 }
